48605e2c783541a15885859df2d4827fa4788fe7
Anuj <anujguptaaj123@gmail.com>
2025-02-03 22:36:22 +0530

    frotend start

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import { React ,useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import UserLogin from './pages/UserLogin'
+import Home from './pages/Home'
+import UserSignUp from './pages/UserSignUp'
+import CaptainLogin from './pages/CaptainLogin'
+import CaptainSignUp from './pages/CaptainSignUp'
+
+
+function App() {
+
+  return (
+    <div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<UserLogin/>}/>
+      <Route path="/signup" element={<UserSignUp/>}/>
+      <Route path="/captain-login" element={<CaptainLogin/>}/>
+      <Route path="/captain-signup" element={<CaptainSignUp/>}/>
+    </Routes>
+    </div>
+  )
+}
+
+export default App
