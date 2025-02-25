f974356308e84ff1c9bcb15e0c071910c11d3741
Anuj <anujguptaaj123@gmail.com>
2025-02-25 17:02:27 +0530

    done

--- Backend/app.js
+++ Backend/app.js
@@ -5,6 +5,8 @@ const app = express();
 const cookieParser = require('cookie-parser');
 const userRoutes = require('./routes/user.routes');
 const captainRoutes = require('./routes/captain.routes')
+const mapRoutes = require('./routes/maps.routes');
+const rideRoutes = require('./routes/ride.routes');
 
 app.use(cors());
 app.use(express.json());
@@ -19,5 +21,7 @@ app.get('/', (req, res) => {
 
 app.use('/users', userRoutes);
 app.use('/captains', captainRoutes)
+app.use('/maps', mapRoutes);
+app.use('/rides', rideRoutes);
 
 module.exports = app;
